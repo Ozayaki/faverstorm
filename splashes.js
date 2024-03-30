6b391d4440675ead9c98dceacbf4894c3ce8fd0b
Ozayaki <156956569+Ozayaki@users.noreply.github.com>
2024-03-30 23:05:27 +0300

    Add files via upload

---
+++ splashes.js
@@ -0,0 +1,17 @@
+let splash = document.getElementById('splash')
+let splash1 = "Имбовый сервер для игры с друзьями!"
+let splash2 = "Почему бы тебе не зайти?"
+let splash3 = "Залетай на сервер! Здесь очень много халявы!"
+
+function splashl() {
+    let rand = Math.ceil(Math.random()*10);
+    if (rand<3) {
+        splash.textContent = splash1
+    }else if (rand>6){
+        splash.textContent = splash2
+    } else {
+        splash.textContent = splash3
+    }
+    
+}
+splashl()
